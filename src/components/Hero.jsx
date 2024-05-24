0afa8ca907257558e941bd867ed8b22659bb32a5
DarkVader121 <128356705+DarkVader121@users.noreply.github.com>
2024-05-25 02:18:12 +0800

    first progress

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+  } from "@material-tailwind/react";
+
+const Hero = () => {
+    return (
+        <div className='flex justify-center items-center h-screen bg-light-blue-500'>
+            <Card className='w-80'>
+                test
+            </Card>
+        </div>
+      );
+}
+
+export default Hero
