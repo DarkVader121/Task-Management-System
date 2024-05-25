d125b0354da41ee792d695fe0af8d4b77b263351
DarkVader121 <128356705+DarkVader121@users.noreply.github.com>
2024-05-25 12:37:08 +0800

    ThingsTodo

--- src/components/ThingsTodo.jsx
+++ src/components/ThingsTodo.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {
     Typography,
     Card,
 } from "@material-tailwind/react";
+import { useStore  } from '../stores'
 
 const ThingsTodo = () => {
+    const { state } = useStore();
+    const { todos } = state;
+
+    useEffect(() => {
+        console.log('Todos have been updated:', todos);
+    }, [todos]);
     return (
      <>
         <div className='flex align-center'>
@@ -13,139 +20,33 @@ const ThingsTodo = () => {
                 Things To Do
             </Typography>
         </div>
-        <div className="p-3">
-            {/* Things To Do  */}
-            <Card className="p-3 w-full bg-gray-200 mt-3">
-                <div className='flex align-center justify-between'>
-                    <div className="flex justify-start align-center">
-                        <Typography  variant="h1" id="Inter" className="font-bold lg:text-2xl">
-                            Review React
-                        </Typography>
-                        <i className="fa-solid fa-pen-to-square text-2xl ml-5 cursor-pointer"></i>
-                    </div>
-                    <i className="fa-solid fa-trash text-2xl ml-5 cursor-pointer text-red-400 mr-3"></i>
-                </div>
-                <Typography  variant="h1" id="Inter" className="lg:text-sm text-red-400 mt-1">
-                    Deadline:  May 13th, 2024
-                </Typography>
+        
+        {todos.length > 0 ? (
+                    todos.map((todo, index) => (
+                        <Card className="p-3 w-full bg-gray-200 mt-3" key={index}>
+                            <div className='flex align-center justify-between'>
+                                <div className="flex justify-start align-center">
+                                    <Typography  variant="h1" id="Inter" className="font-bold lg:text-2xl">
+                                        {todo.title}
+                                    </Typography>
+                                    <i className="fa-solid fa-pen-to-square text-2xl ml-5 cursor-pointer"></i>
+                                </div>
+                                <i className="fa-solid fa-trash text-2xl ml-5 cursor-pointer text-red-400 mr-3"></i>
+                            </div>
+                            <Typography  variant="h1" id="Inter" className="lg:text-sm text-red-400 mt-1">
+                                {`Deadline: ${todo.deadline}`}
+                            </Typography>
 
-                <Typography  variant="h1" id="Inter" className="lg:text-base mt-3">
-                Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
-                print, graphic or web designs. The passage is attributed to an unknown typesetter 
-                ﻿in the 15th century who is thought
-                </Typography>
-            </Card>
-
-                {/* Things To Do  */}
-                <Card className="p-3 w-full bg-gray-200 mt-3">
-                <div className='flex align-center justify-between'>
-                    <div className="flex justify-start align-center">
-                        <Typography  variant="h1" id="Inter" className="font-bold lg:text-2xl">
-                            Review React
-                        </Typography>
-                        <i className="fa-solid fa-pen-to-square text-2xl ml-5 cursor-pointer"></i>
-                    </div>
-                    <i className="fa-solid fa-trash text-2xl ml-5 cursor-pointer text-red-400 mr-3"></i>
-                </div>
-                <Typography  variant="h1" id="Inter" className="lg:text-sm text-red-400 mt-1">
-                    Deadline:  May 13th, 2024
-                </Typography>
-
-                <Typography  variant="h1" id="Inter" className="lg:text-base mt-3">
-                Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
-                print, graphic or web designs. The passage is attributed to an unknown typesetter 
-                ﻿in the 15th century who is thought
-                </Typography>
-            </Card>
-
-                {/* Things To Do  */}
-                <Card className="p-3 w-full bg-gray-200 mt-3">
-                <div className='flex align-center justify-between'>
-                    <div className="flex justify-start align-center">
-                        <Typography  variant="h1" id="Inter" className="font-bold lg:text-2xl">
-                            Review React
-                        </Typography>
-                        <i className="fa-solid fa-pen-to-square text-2xl ml-5 cursor-pointer"></i>
-                    </div>
-                    <i className="fa-solid fa-trash text-2xl ml-5 cursor-pointer text-red-400 mr-3"></i>
-                </div>
-                <Typography  variant="h1" id="Inter" className="lg:text-sm text-red-400 mt-1">
-                    Deadline:  May 13th, 2024
-                </Typography>
-
-                <Typography  variant="h1" id="Inter" className="lg:text-base mt-3">
-                Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
-                print, graphic or web designs. The passage is attributed to an unknown typesetter 
-                ﻿in the 15th century who is thought
-                </Typography>
-            </Card>
-
-                {/* Things To Do  */}
-                <Card className="p-3 w-full bg-gray-200 mt-3">
-                <div className='flex align-center justify-between'>
-                    <div className="flex justify-start align-center">
-                        <Typography  variant="h1" id="Inter" className="font-bold lg:text-2xl">
-                            Review React
-                        </Typography>
-                        <i className="fa-solid fa-pen-to-square text-2xl ml-5 cursor-pointer"></i>
-                    </div>
-                    <i className="fa-solid fa-trash text-2xl ml-5 cursor-pointer text-red-400 mr-3"></i>
-                </div>
-                <Typography  variant="h1" id="Inter" className="lg:text-sm text-red-400 mt-1">
-                    Deadline:  May 13th, 2024
-                </Typography>
-
-                <Typography  variant="h1" id="Inter" className="lg:text-base mt-3">
-                Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
-                print, graphic or web designs. The passage is attributed to an unknown typesetter 
-                ﻿in the 15th century who is thought
-                </Typography>
-            </Card>
-
-                {/* Things To Do  */}
-                <Card className="p-3 w-full bg-gray-200 mt-3">
-                <div className='flex align-center justify-between'>
-                    <div className="flex justify-start align-center">
-                        <Typography  variant="h1" id="Inter" className="font-bold lg:text-2xl">
-                            Review React
-                        </Typography>
-                        <i className="fa-solid fa-pen-to-square text-2xl ml-5 cursor-pointer"></i>
-                    </div>
-                    <i className="fa-solid fa-trash text-2xl ml-5 cursor-pointer text-red-400 mr-3"></i>
-                </div>
-                <Typography  variant="h1" id="Inter" className="lg:text-sm text-red-400 mt-1">
-                    Deadline:  May 13th, 2024
-                </Typography>
-
-                <Typography  variant="h1" id="Inter" className="lg:text-base mt-3">
-                Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
-                print, graphic or web designs. The passage is attributed to an unknown typesetter 
-                ﻿in the 15th century who is thought
-                </Typography>
-            </Card>
-
-                {/* Things To Do  */}
-                <Card className="p-3 w-full bg-gray-200 mt-3">
-                <div className='flex align-center justify-between'>
-                    <div className="flex justify-start align-center">
-                        <Typography  variant="h1" id="Inter" className="font-bold lg:text-2xl">
-                            Review React
-                        </Typography>
-                        <i className="fa-solid fa-pen-to-square text-2xl ml-5 cursor-pointer"></i>
-                    </div>
-                    <i className="fa-solid fa-trash text-2xl ml-5 cursor-pointer text-red-400 mr-3"></i>
-                </div>
-                <Typography  variant="h1" id="Inter" className="lg:text-sm text-red-400 mt-1">
-                    Deadline:  May 13th, 2024
-                </Typography>
-
-                <Typography  variant="h1" id="Inter" className="lg:text-base mt-3">
-                Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
-                print, graphic or web designs. The passage is attributed to an unknown typesetter 
-                ﻿in the 15th century who is thought
-                </Typography>
-            </Card>
-        </div>
+                            <Typography  variant="h1" id="Inter" className="lg:text-base mt-3">
+                                {todo.description}
+                            </Typography>
+                        </Card>
+                    ))
+                ) : (
+                    <Typography variant="h6" className="mt-4">
+                        No todos yet!
+                    </Typography>
+                )}
      </>
       );
 }
